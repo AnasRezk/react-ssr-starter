0b4f2b6112f6563d0f95fbdb26600d24e0ef5663
anasrezk <anas.rezk.js@gmail.com>
2019-04-22 12:37:15 +0200

    addredux logger

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers, {});
+const middleware = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = require('redux-logger');
+  const logger = createLogger({ collapsed: true });
+  middleware.push(logger);
+}
+
+const store = createStore(reducers, {}, applyMiddleware(...middleware));
 
 export default store;
