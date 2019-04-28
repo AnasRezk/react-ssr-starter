9de0837a84cdf1afbe69625b746dd11358d8136c
anasrezk <anas.rezk.js@gmail.com>
2019-04-28 02:19:57 +0200

    add fack test case

--- src/guest/app.js
+++ src/guest/app.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const GuestApp = () => (
     <div className="app">
-        <nav>
-            <NavLink exact to="/" activeClassName="active">
-                New Home
-            </NavLink>{' '}
-            <NavLink exact to="/about" activeClassName="active">
-                New About
-            </NavLink>
-        </nav>
-
         <div className="main">guest App Is New</div>
-
         <footer />
     </div>
 );
